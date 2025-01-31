16684016fd478555ed8e50d19d833373f65ddee5
Alohaoff <andreaaloi5@icloud.com>
2025-01-31 16:48:10 +0100

    Terzo Caricamento

--- EsercizioRiassuntivopt2.py
+++ EsercizioRiassuntivopt2.py
@@ -1,34 +1,39 @@
+def quiz():
 #Domanda 1
-for tentativo in range(3):
-    print("Qual è la capitale dell'Italia?")
-    risposta = input()
-    if risposta.strip().lower() == "roma":
-        print("Esatto!")
-        break
+    for tentativo in range(2):
+        print("Qual è la capitale dell'Italia?")
+        risposta = input()
+        if risposta.strip().lower() == "roma":
+            print("Esatto!")
+            break
+        else:
+            print("Risposta errata!")
     else:
-        print("Risposta errata!")
-else:
-    print("Hai esaurito i tentativi! La risposta corretta era 'Roma'.")
+        print("Hai esaurito i tentativi! La risposta corretta era 'Roma'.")
+        return
     
-#Domanda 2
-for tentativo in range(3):
-    print("Dove si trova Berlino?")
-    risposta = input()
-    if risposta.strip().lower() == "germania":
-        print("Esatto!")
-        break
+    #Domanda 2
+    for tentativo in range(2):
+        print("Dove si trova Berlino?")
+        risposta = input()
+        if risposta.strip().lower() == "germania":
+            print("Esatto!")
+            break
+        else:
+            print("Risposta errata!")
     else:
-        print("Risposta errata!")
-else:
-    print("Hai esaurito i tentativi! La risposta corretta era 'Germania'.")
-#Domanda 3
-for tentativo in range(3):
-    print("Barcellona è la capitale della Spagna? (Sì/No)")
-    risposta = input()
-    if risposta.strip().lower() == "no":
-        print("Esatto!")
-        break
+        print("Hai esaurito i tentativi! La risposta corretta era 'Germania'.")
+        return
+    
+    #Domanda 3
+    for tentativo in range(1):
+        print("Barcellona è la capitale della Spagna? (Sì/No)")
+        risposta = input()
+        if risposta.strip().lower() == "no":
+            print("Esatto!")
+            break
+        else:
+            print("Risposta errata! Tentativi rimasti:")
     else:
-        print("Risposta errata! Tentativi rimasti:")
-else:
-    print("Hai esaurito i tentativi! La risposta corretta era 'No'.")
+        print("Hai esaurito i tentativi! La risposta corretta era 'No'.")
+        return
